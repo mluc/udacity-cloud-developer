903a2ee713bd706e983a0f2578f2a0c1f09107ed
mluc <my.luc@opcity.com>
2020-04-27 23:58:07 -0500

    temp

--- udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ udacity-c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -49,7 +49,7 @@ router.get('/:id',
 
 // update a specific resource
 router.patch('/:id', 
-    requireAuth, 
+    requireAuth,
     async (req: Request, res: Response) => {
         //@TODO try it yourself
         res.send(500).send("not implemented")
@@ -57,11 +57,13 @@ router.patch('/:id',
 
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
+router.get('/signed-url/:fileName',
+    requireAuth,
     async (req: Request, res: Response) => {
+    console.log('++++++++ in signed-url/:fileName')
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
+    console.log('++++++++ in signed-url/:fileName URL', url)
     res.status(201).send({url: url});
 });
 
@@ -69,7 +71,7 @@ router.get('/signed-url/:fileName',
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
 router.post('/', 
-    requireAuth, 
+    requireAuth,
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;

714cfe5401ff659c1243cb5355fba88495377b33
mluc <my.luc@opcity.com>
2020-05-03 16:05:34 -0500

    config

--- udacity-c3-restapi-user/src/config/config.ts
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -11,11 +11,15 @@ export const config = {
     'url': 'http://localhost:8100'
   },
   'prod': {
-    'username': '',
-    'password': '',
-    'database': 'udagram_prod',
-    'host': '',
-    'dialect': 'postgres'
+    'username': process.env.POSTGRESS_USERNAME,
+    'password': process.env.POSTGRESS_PASSWORD,
+    'database': process.env.POSTGRESS_DB,
+    'host': process.env.POSTGRESS_HOST,
+    'dialect': 'postgres',
+    'aws_reigion': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_BUCKET,
+    'url': 'http://localhost:8100'
   },
   'jwt': {
     'secret': process.env.JWT_SECRET

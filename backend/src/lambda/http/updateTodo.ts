59317645ca552b084330cc7e5993019d25970d96
mluc <my.luc@opcity.com>
2020-05-15 16:28:05 -0500

    update working

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,47 @@
-// import 'source-map-support/register'
-//
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-//
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-//
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-//
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import * as AWS from 'aws-sdk';
+
+const todosTable = process.env.TODOS_TABLE
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event)
+    const todoId = event.pathParameters.todoId;
+    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body);
+
+    await docClient.update({
+        TableName: todosTable,
+        Key: {todoId: todoId}, //TODO: add userId
+        UpdateExpression: "set #n = :n, dueDate=:dd, done=:d",
+        ExpressionAttributeValues: {
+            ":n": updatedTodo.name,
+            ":dd": updatedTodo.dueDate,
+            ":d": updatedTodo.done
+        },
+        ExpressionAttributeNames: {
+            "#n": "name"
+        },
+        ReturnValues:"UPDATED_NEW"
+    }).promise();
+
+    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+        })
+    }
+});
+
+handler.use(
+    cors({
+        credentials: true
+    })
+)

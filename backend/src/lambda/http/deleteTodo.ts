0cdb4048d121e8565e3a8f42dcd4addc5f62ccbe
mluc <my.luc@opcity.com>
2020-05-15 15:04:59 -0500

    delete working

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,33 @@
-// import 'source-map-support/register'
-//
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-//
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
+import * as middy from 'middy';
+import {cors} from 'middy/middlewares';
+import * as AWS from 'aws-sdk';
+
+const todosTable = process.env.TODOS_TABLE
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event)
+    const todoId = event.pathParameters.todoId
+
+  // TODO: Remove a TODO item by id
+    await docClient.delete({
+        TableName: todosTable,
+        Key: {todoId: todoId} //TODO: add userId
+    }).promise()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+        })
+    }
+})
+
+handler.use(
+    cors({
+        credentials: true
+    })
+)

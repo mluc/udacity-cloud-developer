c4dcf3680ac27e38aeacb198913d8646b6a67f58
mluc <my.luc@opcity.com>
2020-05-15 01:45:45 -0500

    working

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,9 +4,34 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+const todosTable = process.env.TODOS_TABLE
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event)
+    const todoId = uuid.v4()
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    const newItem = {
+        todoId: todoId,
+        ...newTodo
+    }
+
+    await docClient.put({
+        TableName: todosTable,
+        Item: newItem
+    }).promise()
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            newItem
+        })
+    }
 }

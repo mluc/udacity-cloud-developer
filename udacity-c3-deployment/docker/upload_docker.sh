8b0351350635d3b3fddae99ac1146d83eef4df3f
mluc <my.luc@opcity.com>
2020-05-16 16:18:43 -0500

    Cleanup for capstone

--- udacity-c3-deployment/docker/upload_docker.sh
+++ udacity-c3-deployment/docker/upload_docker.sh
@@ -7,21 +7,21 @@ sudo docker system prune -f
 cd udacity-c3-deployment/docker/
 pwd
 
-dockerpath=myluc/udacity-restapi-feed:V3
+dockerpath=myluc/udacity-restapi-feed:V0
 sudo docker build -t $dockerpath ../../udacity-c3-restapi-feed
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/udacity-restapi-user:V3
+dockerpath=myluc/udacity-restapi-user:V0
 sudo docker build -t $dockerpath ../../udacity-c3-restapi-user
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/udacity-frontend:V8
+dockerpath=myluc/udacity-frontend:V0
 sudo docker build -t $dockerpath ../../udacity-c3-frontend
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/reverseproxy:V3
+dockerpath=myluc/reverseproxy:V0
 sudo docker build -t $dockerpath .
 sudo docker push $dockerpath

f1520a2cadea609553dcc37c2a785e14df7bb72d
mluc <my.luc@opcity.com>
2020-05-15 23:08:04 -0500

    composite key

--- backend/src/dataLayer/todosAccess.ts
+++ backend/src/dataLayer/todosAccess.ts
@@ -7,6 +7,7 @@ export class TodoAccess {
 
     constructor(
         private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly indexName = process.env.INDEX_NAME,
         private readonly todosTable = process.env.TODOS_TABLE) {
     }
 
@@ -14,8 +15,13 @@ export class TodoAccess {
         console.log('Data Layer getAllTodoItems', userId);
 
         // TODO: use query to get todo items from user id
-        const result = await this.docClient.scan({
-            TableName: this.todosTable
+        const result = await this.docClient.query({
+            TableName: this.todosTable,
+            IndexName: this.indexName,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
+            }
         }).promise();
 
         const items = result.Items;
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '5yitjll920'
+const apiId = '6bi8w75bgk'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {

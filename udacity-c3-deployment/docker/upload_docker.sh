a3d0e07cf1847cb966563050f83db26a65689c8d
mluc <my.luc@opcity.com>
2020-05-16 16:48:24 -0500

    V1

--- udacity-c3-deployment/docker/upload_docker.sh
+++ udacity-c3-deployment/docker/upload_docker.sh
@@ -7,21 +7,21 @@ sudo docker system prune -f
 cd udacity-c3-deployment/docker/
 pwd
 
-dockerpath=myluc/udacity-restapi-feed:V0
+dockerpath=myluc/udacity-restapi-feed:V1
 sudo docker build -t $dockerpath ../../udacity-c3-restapi-feed
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/udacity-restapi-user:V0
+dockerpath=myluc/udacity-restapi-user:V1
 sudo docker build -t $dockerpath ../../udacity-c3-restapi-user
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/udacity-frontend:V0
+dockerpath=myluc/udacity-frontend:V1
 sudo docker build -t $dockerpath ../../udacity-c3-frontend
 sudo docker push $dockerpath
 
 
-dockerpath=myluc/reverseproxy:V0
+dockerpath=myluc/reverseproxy:V1
 sudo docker build -t $dockerpath .
 sudo docker push $dockerpath

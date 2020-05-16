12ab325dc178bb7613ac6e3ba1aa1188cc4cf117
mluc <my.luc@opcity.com>
2020-05-15 21:28:33 -0500

    temp

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,96 @@
-// import 'source-map-support/register'
+// import 'source-map-support/register';
 //
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+// import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
+// import * as AWS  from 'aws-sdk'
+// import * as uuid from 'uuid'
+// import * as middy from 'middy';
+// import {cors} from 'middy/middlewares';
 //
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+// const todosTable = process.env.TODOS_TABLE
+// const bucketName = process.env.IMAGES_S3_BUCKET
+// const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 //
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
+// const docClient = new AWS.DynamoDB.DocumentClient()
+// const s3 = new AWS.S3({
+//     signatureVersion: 'v4'
+// })
+//
+//
+// export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//     console.log('Processing event: ', event);
+//     const todoId = event.pathParameters.todoId;
+//     const validTodoId = await todoExists(todoId)
+//     if (!validTodoId) {
+//         return {
+//             statusCode: 404,
+//             body: JSON.stringify({
+//                 error: 'Todo does not exist'
+//             })
+//         }
+//     }
+//
+//     const imageId = uuid.v4()
+//     //const newItem = await createImage(todoId, imageId, event)
+//
+//     const url = getUploadUrl(imageId)
+//
+//
+//     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+//     return {
+//         statusCode: 201,
+//         body: JSON.stringify({
+//             attachmentUrl: url
+//         })
+//     }
+// });
+//
+// handler.use(
+//     cors({
+//         credentials: true
+//     })
+// );
+//
+// async function todoExists(todoId: string) {
+//     const result = await docClient
+//         .get({
+//             TableName: todosTable,
+//             Key: {
+//                 id: todoId
+//             }
+//         })
+//         .promise()
+//
+//     console.log('Get todo: ', result)
+//     return !!result.Item
+// }
+//
+// // async function createImage(todoId: string, imageId: string, event: any) {
+// //     const timestamp = new Date().toISOString()
+// //     const newImage = JSON.parse(event.body)
+// //
+// //     const newItem = {
+// //         groupId,
+// //         timestamp,
+// //         imageId,
+// //         ...newImage,
+// //         imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`
+// //     }
+// //     console.log('Storing new item: ', newItem)
+// //
+// //     await docClient
+// //         .put({
+// //             TableName: imagesTable,
+// //             Item: newItem
+// //         })
+// //         .promise()
+// //
+// //     return newItem
+// // }
+//
+// function getUploadUrl(imageId: string) {
+//     return s3.getSignedUrl('putObject', {
+//         Bucket: bucketName,
+//         Key: imageId,
+//         Expires: urlExpiration
+//     })
 // }
